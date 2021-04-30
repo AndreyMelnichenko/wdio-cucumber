418fbc26970ae62fa904042e1509dc88ebc0ec07
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2021-04-30 12:59:32 +0300

    Fix for WORLD context

--- src/step-definitions/steps.ts
+++ src/step-definitions/steps.ts
@@ -4,23 +4,23 @@ import SecurePage from '../pages/secure.page';
 
 const loginPage:LoginPage = new LoginPage();
 
-Given(/^I am on the (\w+) page$/, async (page: string) => {
+Given(/^I am on the (\w+) page$/, async function (page: string) {
     console.log("I'll open "+page);
     await loginPage.open();
 });
 
-When(/^I login with (\w+) and (.+)$/, async (username:string, password:string) => {
+When(/^I login with (\w+) and (.+)$/, async function (username:string, password:string) {
     await loginPage.login(username, password)
 });
 
-Then(/^I should see a flash message saying (.*)$/, async (message:string) => {
+Then(/^I should see a flash message saying (.*)$/, async function (message:string) {
     await expect(SecurePage.flashAlert).toBeExisting();
     await expect(SecurePage.flashAlert).toHaveTextContaining(message);
     console.log("!!!!!!!!!!");
     // await new Promise(resolve => setTimeout(resolve, 5 * 1000));
 });
 
-Then(/^Test step$/, (table: DataTable) => {
+Then(/^Test step$/, async function (table: DataTable) {
     // const world: CustomWorld<any> = new CustomWorld<any>();
     const a: any = table.hashes()[0]
     this.keepValue("Test", a);
